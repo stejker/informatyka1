6a7af67e71ab93e1b9f8cef7fee5f3f50a0eacd1
stejker <73596117+stejker@users.noreply.github.com>
2020-11-03 18:39:32 +0100

    Add files via upload

---
+++ zad9.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  3 17:23:21 2020
+
+@author: piecza95
+"""
+d=0
+for a in range(-100,100):
+    c=((a-1)^2)-(4*((-a)^3)*((a^2)+1))
+    if c>=0:
+        d +=1
+print("liczba wyników rzeczywistych:",d)
